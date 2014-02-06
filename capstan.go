9915db66ae039cb56e3b68e7082230a425a69d4e
Pekka Enberg <penberg@cloudius-systems.com>
2014-02-06 16:55:04 +0200

    Add license banner to files
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- capstan.go
+++ capstan.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
 package main
 
 import "github.com/cloudius-systems/capstan/repository"
