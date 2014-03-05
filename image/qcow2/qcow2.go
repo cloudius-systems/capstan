f015c7b3d8f5e264da18a3f46518a4e61aed2053
Pekka Enberg <penberg@cloudius-systems.com>
2014-03-05 14:01:27 +0200

    Add Cloudius copyright statements
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- image/qcow2/qcow2.go
+++ image/qcow2/qcow2.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
 package qcow2
 
 import (
