bc654359dd0229a5437c89e7dc6637db81ec33d1
Pekka Enberg <penberg@cloudius-systems.com>
2014-03-05 11:56:34 +0200

    Add support for running user-specified images
    Add support for running images that are not in the Capstan repository.
    This is useful for testing hypervisor support and running Fedora images,
    for example.
    
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- cmd/run.go
+++ cmd/run.go
@@ -3,22 +3,37 @@ package cmd
 import (
 	"fmt"
 	"github.com/cloudius-systems/capstan"
+	"github.com/cloudius-systems/capstan/image"
 	"github.com/cloudius-systems/capstan/qemu"
+	"os"
 )
 
-func Run(repo *capstan.Repo, verbose bool, image string) {
-	if !repo.ImageExists(image) {
-		if !capstan.ConfigExists("Capstanfile") {
-			fmt.Printf("%s: no such image\n", image)
+func Run(repo *capstan.Repo, verbose bool, imageName string) {
+	var path string
+	file, err := os.Open(imageName)
+	if err == nil {
+		path = imageName
+		format := image.Probe(file)
+		if format == image.Unknown {
+			file.Close()
+			fmt.Printf("%s: image format not recognized, unable to run it.\n", path)
 			return
 		}
-		err := qemu.BuildImage(repo, image, verbose)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
+		file.Close()
+	} else {
+		if !repo.ImageExists(imageName) {
+			if !capstan.ConfigExists("Capstanfile") {
+				fmt.Printf("%s: no such image\n", imageName)
+				return
+			}
+			err := qemu.BuildImage(repo, imageName, verbose)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
 		}
+		path = repo.ImagePath(imageName)
 	}
-	file := repo.ImagePath(image)
-	cmd := qemu.LaunchVM(true, file)
+	cmd := qemu.LaunchVM(true, path)
 	cmd.Wait()
 }
