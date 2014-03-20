cloudius-systems/capstan

commit 8e3735419fc464131cfe0494a8c85539107392c6

Per-hypervisor images
Start using per-hypervisor images in the Capstan repository.  This is
needed for vbox image build support.  Please note that QEMU is still
used for the actual image building but the generated image is in VDI
format for vbox.

Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

repository.go
@@ -10,6 +10,7 @@ package capstan
 import (
 	"errors"
 	"fmt"
+	"github.com/cloudius-systems/capstan/image"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -32,12 +33,12 @@ func NewRepo() *Repo {
 }
 
 func (r *Repo) PullImage(image string) error {
-	if r.ImageExists(image) {
+	workTree := r.workTree(image)
+	if _, err := os.Stat(workTree); os.IsExist(err) {
 		return r.updateImage(image)
 	}
 	fmt.Printf("Pulling %s...\n", image)
 	gitUrl := fmt.Sprintf("https://github.com/%s", image)
-	workTree := r.workTree(image)
 	cmd := exec.Command("git", "clone", "--depth", "1", gitUrl, workTree)
 	_, err := cmd.Output()
 	if err != nil {
@@ -68,17 +69,32 @@ func (r *Repo) workTree(image string) string {
 	return filepath.Join(r.Path, image)
 }
 
-func (r *Repo) PushImage(image string, file string) error {
+func (r *Repo) PushImage(imageName string, file string) error {
+	format, err := image.Probe(file)
+	if err != nil {
+		return err
+	}
+	var hypervisor string
+	switch format {
+	case image.VDI:
+		hypervisor = "vbox"
+	case image.QCOW2:
+		hypervisor = "qemu"
+	case image.VMDK:
+		hypervisor = "vmware"
+	default:
+		return fmt.Errorf("%s: unsupported image format", file)
+	}
 	if _, err := os.Stat(file); os.IsNotExist(err) {
 		return errors.New(fmt.Sprintf("%s: no such file", file))
 	}
-	fmt.Printf("Pushing %s...\n", image)
-	cmd := exec.Command("mkdir", "-p", filepath.Dir(r.ImagePath(image)))
-	_, err := cmd.Output()
+	fmt.Printf("Pushing %s...\n", imageName)
+	cmd := exec.Command("mkdir", "-p", filepath.Dir(r.ImagePath(hypervisor, imageName)))
+	_, err = cmd.Output()
 	if err != nil {
-		return errors.New(fmt.Sprintf("%s: mkdir failed", filepath.Dir(r.ImagePath(image))))
+		return errors.New(fmt.Sprintf("%s: mkdir failed", filepath.Dir(r.ImagePath(hypervisor, imageName))))
 	}
-	cmd = exec.Command("cp", file, r.ImagePath(image))
+	cmd = exec.Command("cp", file, r.ImagePath(hypervisor, imageName))
 	_, err = cmd.Output()
 	if err != nil {
 		return err
@@ -86,8 +102,8 @@ func (r *Repo) PushImage(image string, file string) error {
 	return nil
 }
 
-func (r *Repo) ImageExists(image string) bool {
-	file := r.ImagePath(image)
+func (r *Repo) ImageExists(hypervisor, image string) bool {
+	file := r.ImagePath(hypervisor, image)
 	if _, err := os.Stat(file); os.IsNotExist(err) {
 		return false
 	}
@@ -95,18 +111,18 @@ func (r *Repo) ImageExists(image string) bool {
 }
 
 func (r *Repo) RemoveImage(image string) error {
-	if !r.ImageExists(image) {
+	path := filepath.Join(r.Path, image)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return errors.New(fmt.Sprintf("%s: no such image\n", image))
 	}
-	file := r.ImagePath(image)
 	fmt.Printf("Removing %s...\n", image)
-	cmd := exec.Command("rm", "-rf", filepath.Dir(file))
+	cmd := exec.Command("rm", "-rf", filepath.Dir(path))
 	_, err := cmd.Output()
 	return err;
 }
 
-func (r *Repo) ImagePath(image string) string {
-	return filepath.Join(r.Path, image, filepath.Base(image))
+func (r *Repo) ImagePath(hypervisor string, image string) string {
+	return filepath.Join(r.Path, image, fmt.Sprintf("%s.%s", filepath.Base(image), hypervisor))
 }
 
 func (r *Repo) ListImages() {

capstan/capstan.go
@@ -116,6 +116,7 @@ func main() {
 			Name:  "build",
 			Usage: "build an image",
 			Flags: []cli.Flag{
+				cli.StringFlag{"p", "qemu", "hypervisor"},
 				cli.BoolFlag{"v", "verbose mode"},
 			},
 			Action: func(c *cli.Context) {
@@ -127,7 +128,8 @@ func main() {
 					fmt.Println("usage: capstan build [image-name]")
 					return
 				}
-				err := cmd.Build(repo, image, c.Bool("v"))
+				hypervisor := c.String("p")
+				err := cmd.Build(repo, hypervisor, image, c.Bool("v"))
 				if err != nil {
 					fmt.Println(err.Error())
 				}

cmd/build.go
@@ -17,13 +17,13 @@ import (
 	"strings"
 )
 
-func Build(r *capstan.Repo, image string, verbose bool) error {
+func Build(r *capstan.Repo, hypervisor string, image string, verbose bool) error {
 	config, err := capstan.ReadConfig("Capstanfile")
 	if err != nil {
 		return err
 	}
 	fmt.Printf("Building %s...\n", image)
-	err = os.MkdirAll(filepath.Dir(r.ImagePath(image)), 0777)
+	err = os.MkdirAll(filepath.Dir(r.ImagePath(hypervisor, image)), 0777)
 	if err != nil {
 		return err
 	}
@@ -38,30 +38,30 @@ func Build(r *capstan.Repo, image string, verbose bool) error {
 			return err
 		}
 	}
-	err = config.Check(r)
+	err = config.Check(r, hypervisor)
 	if err != nil {
 		return err
 	}
-	cmd := exec.Command("cp", r.ImagePath(config.Base), r.ImagePath(image))
+	cmd := exec.Command("cp", r.ImagePath(hypervisor, config.Base), r.ImagePath(hypervisor, image))
 	_, err = cmd.Output()
 	if err != nil {
 		return err
 	}
-	err = qemu.SetArgs(r, image, "/tools/cpiod.so")
+	err = qemu.SetArgs(r, hypervisor, image, "/tools/cpiod.so")
 	if err != nil {
 		return err
 	}
 	if config.RpmBase != nil {
-		err = qemu.UploadRPM(r, image, config, verbose)
+		err = qemu.UploadRPM(r, hypervisor, image, config, verbose)
 		if err != nil {
 			return err
 		}
 	}
-	err = qemu.UploadFiles(r, image, config, verbose)
+	err = qemu.UploadFiles(r, hypervisor, image, config, verbose)
 	if err != nil {
 		return err
 	}
-	err = qemu.SetArgs(r, image, config.Cmdline)
+	err = qemu.SetArgs(r, hypervisor, image, config.Cmdline)
 	if err != nil {
 		return err
 	}

cmd/run.go
@@ -29,16 +29,16 @@ type RunConfig struct {
 func Run(repo *capstan.Repo, config *RunConfig) error {
 	var path string
 	if _, err := os.Stat(config.ImageName); os.IsNotExist(err) {
-		if !repo.ImageExists(config.ImageName) {
+		if !repo.ImageExists(config.Hypervisor, config.ImageName) {
 			if !capstan.ConfigExists("Capstanfile") {
 				return fmt.Errorf("%s: no such image", config.ImageName)
 			}
-			err := Build(repo, config.ImageName, config.Verbose)
+			err := Build(repo, config.Hypervisor, config.ImageName, config.Verbose)
 			if err != nil {
 				return err
 			}
 		}
-		path = repo.ImagePath(config.ImageName)
+		path = repo.ImagePath(config.Hypervisor, config.ImageName)
 	} else {
 		path = config.ImageName
 	}

config.go
@@ -8,10 +8,10 @@
 package capstan
 
 import (
-	"fmt"
-	"os"
 	"errors"
+	"fmt"
 	"github.com/kylelemons/go-gypsy/yaml"
+	"os"
 	"strings"
 )
 
@@ -90,8 +90,8 @@ func ParseConfig(config *yaml.File) (*Config, error) {
 	return result, nil
 }
 
-func (config *Config) Check(r *Repo) error {
-	if _, err := os.Stat(r.ImagePath(config.Base)); os.IsNotExist(err) {
+func (config *Config) Check(r *Repo, hypervisor string) error {
+	if _, err := os.Stat(r.ImagePath(hypervisor, config.Base)); os.IsNotExist(err) {
 		return r.PullImage(config.Base)
 	}
 	for _, value := range config.Files {

hypervisor/qemu/qemu.go
@@ -29,8 +29,8 @@ type VMConfig struct {
 	Redirects []string
 }
 
-func UploadRPM(r *capstan.Repo, image string, config *capstan.Config, verbose bool) error {
-	file := r.ImagePath(image)
+func UploadRPM(r *capstan.Repo, hypervisor string, image string, config *capstan.Config, verbose bool) error {
+	file := r.ImagePath(hypervisor, image)
 	vmconfig := &VMConfig{
 		Image:     file,
 		Verbose:   verbose,
@@ -65,8 +65,8 @@ func UploadRPM(r *capstan.Repo, image string, config *capstan.Config, verbose bo
 	return err
 }
 
-func UploadFiles(r *capstan.Repo, image string, config *capstan.Config, verbose bool) error {
-	file := r.ImagePath(image)
+func UploadFiles(r *capstan.Repo, hypervisor string, image string, config *capstan.Config, verbose bool) error {
+	file := r.ImagePath(hypervisor, image)
 	vmconfig := &VMConfig{
 		Image:     file,
 		Verbose:   verbose,
@@ -102,8 +102,8 @@ func UploadFiles(r *capstan.Repo, image string, config *capstan.Config, verbose 
 	return cmd.Wait()
 }
 
-func SetArgs(r *capstan.Repo, image string, args string) error {
-	file := r.ImagePath(image)
+func SetArgs(r *capstan.Repo, hypervisor, image string, args string) error {
+	file := r.ImagePath(hypervisor, image)
 	cmd := exec.Command("qemu-nbd", file)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {

commit 4906920590f34862b9f2f1071b8f0a9f5f5e5b31

pull: Fix updating an existing image
The check in PullImage() was broken by commit 8e37354 ("Per-hypervisor
images").  Fix it up.

Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

repository.go
@@ -34,10 +34,19 @@ func NewRepo() *Repo {
 
 func (r *Repo) PullImage(image string) error {
 	workTree := r.workTree(image)
-	if _, err := os.Stat(workTree); os.IsExist(err) {
-		return r.updateImage(image)
+	_, err := os.Stat(workTree)
+	if os.IsNotExist(err) {
+		return r.cloneImage(image)
 	}
+	if err != nil {
+		return err
+	}
+	return r.updateImage(image)
+}
+
+func (r *Repo) cloneImage(image string) error {
 	fmt.Printf("Pulling %s...\n", image)
+	workTree := r.workTree(image)
 	gitUrl := fmt.Sprintf("https://github.com/%s", image)
 	cmd := exec.Command("git", "clone", "--depth", "1", gitUrl, workTree)
 	out, err := cmd.CombinedOutput()