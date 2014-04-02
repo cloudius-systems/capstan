100e02bf3e3de3ca5ba5c2ae2a4e76f6e78f9b78
Pekka Enberg <penberg@cloudius-systems.com>
2014-04-02 15:59:40 +0300

    Platform specific default hypervisors
    Use platform specific hypervisors:
    
     * QEMU/KVM on Linux
     * VirtualBox on OS X and Windows
    
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

---
+++ hypervisor/default_windows.go
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+package hypervisor
+
+func Default() string {
+	return "vbox"
+}
