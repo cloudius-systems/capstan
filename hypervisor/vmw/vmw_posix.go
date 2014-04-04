fab8bca071b974646624216dd93c8de5792f40cb
Asias He <asias@cloudius-systems.com>
2014-04-04 09:43:30 +0800

    vmw: Support VMware Worksation on Windows
    Tested on VMWare Worksation on Windows/Linux and VMware Fusion on Mac
    
    Note: VMware Player does not contain vmrun which required by capstan, so
    we do not support VMware Player.
    
    Fixes #53.
    
    Signed-off-by: Asias He <asias@cloudius-systems.com>

---
+++ hypervisor/vmw/vmw_posix.go
@@ -0,0 +1,18 @@
+// +build linux darwin
+
+/*
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+package vmw
+
+import (
+	"net"
+)
+
+func Connect(path string) (net.Conn, error) {
+	return net.Dial("unix", path)
+}
