60cdbac4ed7f20540bd4d0f523457c9240d488f5
Asias He <asias@cloudius-systems.com>
2014-04-30 13:59:29 +0800

    cmd/delete: Stop instance before delete it
    Signed-off-by: Asias He <asias@cloudius-systems.com>

--- cmd/delete.go
+++ cmd/delete.go
@@ -17,24 +17,27 @@ import (
 )
 
 func Delete(name string) error {
+	var err error
 	instanceName, instancePlatform := util.SearchInstance(name)
 	if instanceName == "" {
 		fmt.Printf("Instance: %s not found\n", name)
 		return nil
 	}
 
-	var err error
 	switch instancePlatform {
 	case "qemu":
+		qemu.StopVM(name)
 		err = qemu.DeleteVM(name)
 	case "vbox":
+		vbox.StopVM(name)
 		err = vbox.DeleteVM(name)
 	case "vmw":
+		vmw.StopVM(name)
 		err = vmw.DeleteVM(name)
 	case "gce":
+		gce.StopVM(name)
 		err = gce.DeleteVM(name)
 	}
-
 	if err != nil {
 		fmt.Printf("Failed to delete instance: %s\n", name)
 		return err
