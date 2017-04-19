de3a31003e3b6d90139bc44da2547e58a36faf05
Miha Pleško <miha.plesko@xlab.si>
2017-04-19 14:53:41 +0200

    Allow user to pass env variables for `capstan run`
    This commit introduces argument `--env` that enables her to
    specify environment variables. Argument is repeatable, e.g.:
    ```
    $ capstan run micro --boot master --env PORT=9292 --env ENDPOINT=kekec.si
    ```
    This simply prepends environment variable stanza to the boot command so
    it can be used both with runscript (--boot) or when providing bootcmd
    directly (--execute).
    
    Change-Id: I2f78f0fef26b1e485dbcc92b025e0f4f92f1edd0
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

---
+++ test/util/parser_test.go
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2017 XLAB, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+package util_test
+
+import (
+	"testing"
+
+	"github.com/mikelangelo-project/capstan/util"
+	. "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type testingParserSuite struct{}
+
+var _ = Suite(&testingParserSuite{})
+
+func (s *testingParserSuite) TestParseEnvironmentList(c *C) {
+	m := []struct {
+		comment     string
+		envList     []string
+		expectedRes map[string]string
+		err         string
+	}{
+		{
+			"no parameters",
+			[]string{},
+			map[string]string{},
+			"",
+		},
+		{
+			"single parameter",
+			[]string{"PORT=8000"},
+			map[string]string{"PORT": "8000"},
+			"",
+		},
+		{
+			"two parameters",
+			[]string{"PORT=8000", "ENDPOINT=foo.com"},
+			map[string]string{"PORT": "8000", "ENDPOINT": "foo.com"},
+			"",
+		},
+		{
+			"invalid char (space) #1",
+			[]string{"NAME=my name"},
+			map[string]string{},
+			"failed to parse --env argument .*",
+		},
+		{
+			"invalid char (space) #2",
+			[]string{"MY NAME=name"},
+			map[string]string{},
+			"failed to parse --env argument .*",
+		},
+		{
+			"invalid char (space) #3",
+			[]string{"MY NAME=my name"},
+			map[string]string{},
+			"failed to parse --env argument .*",
+		},
+		{
+			"value with equals sign #1",
+			[]string{"NAME=my=name"},
+			map[string]string{"NAME": "my=name"},
+			"",
+		},
+		{
+			"value with equals sign #2",
+			[]string{"NAME==name"},
+			map[string]string{"NAME": "=name"},
+			"",
+		},
+		{
+			"one parameter ok, other not",
+			[]string{"PORT=8000", "ENDPOINT=i am invalid"},
+			map[string]string{},
+			"failed to parse --env argument .*",
+		},
+		{
+			"same parameter two times",
+			[]string{"PORT=8000", "PORT=9999"},
+			map[string]string{"PORT": "9999"},
+			"",
+		},
+		{
+			"empty value",
+			[]string{"PORT="},
+			map[string]string{"PORT": ""},
+			"",
+		},
+		{
+			"not key=value format",
+			[]string{"PORT"},
+			map[string]string{},
+			"failed to parse --env argument .*",
+		},
+	}
+	for i, args := range m {
+		c.Logf("CASE #%d: %s", i, args.comment)
+
+		// This is what we're testing here.
+		res, err := util.ParseEnvironmentList(args.envList)
+
+		// Expectations.
+		if args.err != "" {
+			c.Check(err, ErrorMatches, args.err)
+		} else {
+			c.Check(res, DeepEquals, args.expectedRes)
+		}
+	}
+}
